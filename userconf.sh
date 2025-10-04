135c787efc06c78e4b326ef7d99730bfa9e97bb8
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2025-10-04 20:32:04 +0100

    Update userconf.sh

--- userconf.sh
+++ userconf.sh
@@ -1,14 +1,3 @@
 #!/bin/bash -e
-
-getUsername() {
-  read -p "Username: " username
-  echo "$username"
-}
-
-getPassword() {
-  echo "$(openssl passwd -6)"
-}
-
-echo "$(getUsername):$(getPassword)" > userconf.txt
-
 touch ssh
+echo "$(read -p "Username: "):$(openssl passwd -6)" > userconf.txt
